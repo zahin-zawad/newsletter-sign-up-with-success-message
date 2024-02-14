4033e72137b60a5ba7b7f9e0b963e69f4db0dafb
Zahin Zawad <zahin.zawad.19@gmail.com>
2024-02-15 02:20:30 +0600

    vanila js project

---
+++ index.js
@@ -0,0 +1,12 @@
+const form = document.querySelector("form");
+
+
+const card_1 = document.querySelector('.container');
+const card_2 = document.querySelector('.container-2');
+
+form.addEventListener("submit" , (e)=>{
+    e.preventDefault();
+    card_1.classList.add("hide");
+    card_2.classList.remove("hide");
+} )
+
